a9deb166965634434babf5cfb2f618e69c72b632
Sergey <80640077+Player4-AB@users.noreply.github.com>
2022-06-21 20:53:42 +0300

    Add files via upload

---
+++ functions/info.js
@@ -0,0 +1,16 @@
+exports.config = {
+  apiKey: "AIzaSyDgHDklz3JDdwH36nNEHXM35c9x3zgUT4k",
+  authDomain: "projectsakura-aafc1.firebaseapp.com",
+  databaseURL: "projectsakura-aafc1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "projectsakura-aafc1",
+  storageBucket: "projectsakura-aafc1.appspot.com",
+  messagingSenderId: "526752469138",
+  appId: "1:526752469138:web:08f6f2df80a4c03acc964b",
+  measurementId: "G-74J81ZPVDN"
+}
+
+exports.oauthKeys = {
+  clientID: "988837411192709130",
+  clientSecret: "CiNOdGl4HLKY6vy-npPVJsSm_z37UPp6",
+  scopes: ["identify", "email", "guilds.join", "guilds"]
+}
